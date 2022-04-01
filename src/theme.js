df3c3ec8218bf79190f826520a8ad5fce9b99f53
Agmar Putra <agmarputra@gmail.com>
2022-04-01 22:44:52 +0700

    Add cache provider and cache provider

--- src/theme.js
+++ src/theme.js
@@ -3,17 +3,23 @@ import { createTheme } from '@mui/material/styles';
 // Create a theme instance.
 const theme = createTheme({
   palette: {
+    common: {
+      main: '#393939'
+    },
     primary: {
-      main: '#1769AA',
+      main: '#1769AA'
+    },
+    warning: {
+      main: '#E9D757'
+    },
+    success: {
+      main: '#297374'
     },
     secondary: {
-      main: '#838383',
-    },
-    textSecondary: {
-      main: '#A2A2A2'
+      main: '#838383'
     },
     error: {
-      main: '#E25141',
+      main: '#FF8552'
     },
   },
 })
