b7d246b095b94b1f4a85dbbf4b5cd7205c24d59d
Agmar Putra <agmarputra@gmail.com>
2022-04-01 22:41:30 +0700

    Add cache provider and cache provider

--- src/theme.js
+++ src/theme.js
@@ -2,27 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 // Create a theme instance.
 const theme = createTheme({
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
-            width: '5px',
-            height: '14px',
-          },
-          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
-            background: "#C82D39",
-            borderRadius: '10px'
-          },
-          "&::-webkit-scrollbar-track, & *::-webkit-scrollbar-track": {
-            backgroundColor: "#C9C9C9",
-            boxShadow: "inset 0 0 5px grey",
-            borderRadius: '10px'
-          },
-        },
-      },
-    },
-  },
   palette: {
     primary: {
       main: '#1769AA',
@@ -37,15 +16,6 @@ const theme = createTheme({
       main: '#E25141',
     },
   },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 950,
-      lg: 1280,
-      xl: 1536,
-    }
-  },
-});
+})
 
-export default theme;
+export default theme
