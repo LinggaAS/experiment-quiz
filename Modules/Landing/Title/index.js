087b6263cf7bdff73ef99b8af3c62bf17a9530df
Agmar Putra <agmarputra@gmail.com>
2022-04-01 23:42:27 +0700

    isr

--- Modules/Landing/Title/index.js
+++ Modules/Landing/Title/index.js
@@ -3,12 +3,10 @@ import { Box, Typography } from '@mui/material'
 
 const Title = () => {
   return (
-    <Fragment>
-      <Box sx={{ textAlign: 'center', marginTop: '40px' }}>
-        <Typography variant="h1" component="h1" fontWeight="bold">QUIZ</Typography>
-        <Typography variant="h1" component="h1" fontSize="6.192rem">Experiment</Typography>
-      </Box>
-    </Fragment>
+    <Box sx={{ textAlign: 'center', marginTop: '40px' }}>
+      <Typography variant="h1" component="h1" fontWeight="bold">QUIZ</Typography>
+      <Typography variant="h1" component="h1" fontSize="6.192rem">Experiment</Typography>
+    </Box>
   )
 }
  
